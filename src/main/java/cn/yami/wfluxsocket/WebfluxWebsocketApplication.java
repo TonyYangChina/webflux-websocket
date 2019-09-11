443a48810532c35dfd265d880326149b52877f32
Yang Ming <476092417@qq.com>
2019-09-11 10:17:12 +0800

    初始化结构和配置

---
+++ src/main/java/cn/yami/wfluxsocket/WebfluxWebsocketApplication.java
@@ -0,0 +1,16 @@
+package cn.yami.wfluxsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yami 2019-9-11 10:10:27
+ */
+@SpringBootApplication
+public class WebfluxWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxWebsocketApplication.class, args);
+    }
+
+}
